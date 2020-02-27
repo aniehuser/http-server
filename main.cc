063b36e136d0dcb8b07dc7533039f54cef04b437
Tony Niehuser <a.niehuser@f5.com>
2020-02-26 17:09:33 -0800

    tabs -> spaces

--- main.cc
+++ main.cc
@@ -7,41 +7,39 @@
 #include <cstring>
 #include "TestInclude.h"
 
-// using namespace std;
-
 #define MAXLINE 255
 
 int main(int argc, char **argv) {
     int ret = printSomething("Hello World!");
 
-    int					listenfd, connfd;
-	struct sockaddr_in	servaddr;
-	char				buff[MAXLINE];
-	time_t				ticks;
+    int                 listenfd, connfd;
+    struct sockaddr_in  servaddr;
+    char                buff[MAXLINE];
+    time_t              ticks;
 
-	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family      = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port        = htons(8080);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family      = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port        = htons(8080);
 
-	if(bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+    if(bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
         printSomething("bind error");
     }
 
-	listen(listenfd, 5);
+    listen(listenfd, 5);
 
-	for ( ; ; ) {
-		connfd = accept(listenfd, NULL, NULL);
+    for ( ; ; ) {
+        connfd = accept(listenfd, NULL, NULL);
 
         ticks = time(NULL);
         snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
         write(connfd, buff, strlen(buff));
 
-		close(connfd);
+        close(connfd);
         break;
-	}
+    }
 
     return ret;
 }
